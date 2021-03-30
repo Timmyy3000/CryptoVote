a0674d691464d40917e161b890ddafef7318839e
Timmyy3000 <59262737+Timmyy3000@users.noreply.github.com>
2021-03-30 07:28:59 +0100

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,6 @@
-window.addEventListener('load', async () => {
-    // Modern dapp browsers...
-    if (window.ethereum) {
-        window.web3 = new Web3(ethereum);
-        try {
-            await ethereum.enable();
-            var accounts= await web3.eth.getAccounts();
-            var option={from: accounts[0] };
-            var myContract = new web3.eth.Contract(abi,contractAddress);
-            myContract.methods.RegisterInstructor('11','Ali')
-            .send(option,function(error,result){
-                if (! error)
-                    console.log(result);
-                else
-                    console.log(error);
-            });
-        } catch (error) {
-            // User denied account access...
-        }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-        window.web3 = new Web3(web3.currentProvider);
-        // Acccounts always exposed
-        web3.eth.sendTransaction({/* ... */});
-    }
-    // Non-dapp browsers...
-    else {
-        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
-    }
-});
+const accounts = web3.eth.accounts;
+
+console.log(accounts)
 
 App = {
   web3Provider: null,
